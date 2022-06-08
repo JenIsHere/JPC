259ce4cfb45fffa5d9c3fc2c2e099125d45de2ff
Jenish Shah <JenishShaleenkumar.Shah@flemingcollege.ca>
2022-06-08 04:56:59 -0400

    changes side bar

--- geojson.js
+++ geojson.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-    document.getElementById("mySidepanel").style.height = "400px";
+    document.getElementById("mySidepanel").style.width = "10%";
+    document.getElementById("mySidepanel").style.height = "25%";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
