52ac991ef3ad45c15e72d69aaab42602f14c716e
Jenish Shah <JenishShaleenkumar.Shah@flemingcollege.ca>
2022-06-08 04:45:00 -0400

    sidepanel try

--- geojson.js
+++ geojson.js
@@ -1,11 +1,13 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("mySidepanel").style.height = "400px";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+
 var airport = {
     "type": "Feature",
     "properties": { 
